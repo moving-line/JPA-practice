77af5751bffbbb149d599e1f7b2157d98b768069
moving-line <movinglinecheck@gmail.com>
2020-03-02 14:46:56 +0900

    Add Order Domain func
    - 주문 도메인 개발 (주문, 주문취소 기능 추가)

---
+++ src/main/java/jpabook/jpashop/service/OrderService.java
@@ -0,0 +1,44 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.Delivery;
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderItem;
+import jpabook.jpashop.domain.item.Item;
+import jpabook.jpashop.repository.ItemRepository;
+import jpabook.jpashop.repository.MemberRepository;
+import jpabook.jpashop.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+@Service
+public class OrderService {
+
+    private final OrderRepository orderRepository;
+    private final MemberRepository memberRepository;
+    private final ItemRepository itemRepository;
+
+    @Transactional
+    public Long order(Long memberId, Long itemId, int count) {
+        Member member = memberRepository.findById(memberId).orElse(null);
+        Item item = itemRepository.findById(itemId).orElse(null);
+        OrderItem orderItem = OrderItem.createOrderItem(item, item.getPrice(), count);
+        Delivery delivery = new Delivery();
+        delivery.setAddress(member.getAddress());
+
+        Order order = Order.createOrder(member, delivery, orderItem);
+
+        orderRepository.save(order);
+        return order.getId();
+    }
+
+    @Transactional
+    public void cancelOrder(Long orderId) {
+        Order order = orderRepository.findById(orderId).orElse(null);
+        order.cancel();
+    }
+
+}
